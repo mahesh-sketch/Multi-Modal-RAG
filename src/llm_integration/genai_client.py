af0c95e210f12bf48b66ea4c92bfdd8903a0352e
mahesh dash <maheshdash84821@gmail.com>
2025-06-26 13:08:29 +0530

    Gen ai and Groq ai integration

---
+++ src/llm_integration/genai_client.py
@@ -0,0 +1,34 @@
+import sys
+from langchain_google_genai import ChatGoogleGenerativeAI
+from configs.settings import settings
+from core.exception import CustomException
+from core.logger import logging
+
+
+def get_genai_client(model_name:str="gemini-2.0-flash", temperature: float = 0.5):
+    """
+    Initialize Google Generative AI model using LangChain wrapper.
+
+    Args:
+        model_name (str): The Gemini model version to use.
+        temperature (float): Temperature setting for output creativity.
+
+    Returns:
+        ChatGoogleGenerativeAI: Initialized model instance.
+    """
+    try:
+        if not settings.GOOGLE_API_KEY:
+            raise CustomException("GOOGLE_API_KEY is not set in the environment variables.")
+        
+        logging.info(f"Initializing Google Generative AI client with model: {model_name} and temperature: {temperature}")
+        
+        return ChatGoogleGenerativeAI(
+            api_key=settings.GOOGLE_API_KEY,
+            model=model_name,
+            temperature=temperature,
+        )
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+model = get_genai_client()
+print(model.invoke("Hello, how are you?").content)  # Example usage to test the client
---
+++ src/llm_integration/groq_client.py
@@ -0,0 +1,30 @@
+import sys
+from langchain_groq import ChatGroq
+from configs.settings import settings
+from core.exception import CustomException
+from core.logger import logging
+
+def get_groq_client(model_name: str = "meta-llama/llama-4-maverick-17b-128e-instruct", temperature: float = 0.5):
+    """
+    Initialize Groq model using LangChain wrapper.
+
+    Args:
+        model_name (str): The Groq model version to use.
+        temperature (float): Temperature setting for output creativity.
+
+    Returns:
+        ChatGroq: Initialized model instance.
+    """
+    try:
+        if not settings.GROQ_API_KEY:
+            raise CustomException("GROQ_API_KEY is not set in the environment variables.")
+        
+        logging.info(f"Initializing Groq client with model: {model_name} and temperature: {temperature}")
+        
+        return ChatGroq(
+            api_key=settings.GROQ_API_KEY,
+            model=model_name,
+            temperature=temperature,
+        )
+    except Exception as e:
+        raise CustomException(e, sys)
